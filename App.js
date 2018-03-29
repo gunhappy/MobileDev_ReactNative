fa8a471db3d70847bfcff6cf90178ffa80b814ce
gunhappy <phasin.s@ku.th>
2018-03-29 12:09:17 +0700

    create register form

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, Image, StyleSheet, Text, TextInput, View, KeyboardAvoidingView } from 'react-native'
 
 import React from 'react'
 import WelcomeScreen from './screen/WelcomeScreen'
+import RegisterScreen from './screen/RegisterScreen'
 
 export default class App extends React.Component {
 
@@ -12,7 +13,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <WelcomeScreen/>
+        <RegisterScreen/>
       </View>
     )
   }
@@ -21,8 +22,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#fff'
   }
 })
--- screen/RegisterScreen.js
+++ screen/RegisterScreen.js
@@ -1,24 +1,78 @@
-import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, Image, StyleSheet, View, Dimensions, TouchableOpacity, Text, KeyboardAvoidingView, TextInput, ScrollView  } from 'react-native'
 
 import React from 'react'
+import { FormLabel, FormInput } from 'react-native-elements'
+
+const logo = require('../assets/logo.png')
+const window = Dimensions.get('window')
+
+const IMAGE_HEIGHT = window.width / 2
 
 export default class RegisterScreen extends React.Component {
 
   constructor(props){
     super(props)
   }
+
+  onRegister() {
+
+  }
   
   render() {
     return (
-      <View style={styles.container}>
-        <Text>RegisterScreen</Text>
-      </View>
+      <ScrollView style={styles.container}> 
+      <KeyboardAvoidingView behavior='padding'>
+        <View>
+          <Image source={logo} style={styles.logo}/>
+        </View>
+        <FormLabel>Enter Email</FormLabel>
+        <FormInput
+          placeholder='Type your email here.'
+        />
+        <FormLabel>Enter Password</FormLabel>
+        <FormInput
+          placeholder='Type your password here.'
+          secureTextEntry={true}
+        />
+        <FormLabel>Confirm Password</FormLabel>
+        <FormInput
+          placeholder='Re-type your password again.'
+          secureTextEntry={true}
+        />
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={ () => this.onRegister() }>
+            <Text style={styles.regisText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    flex: 1
+  },
+  logo: {
+    height: IMAGE_HEIGHT,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginBottom: 30,
+    marginTop: 50
+  },
+  buttonContainer: {
+    marginTop: 40,
+    width: '80%',
+    height: 60,
+    backgroundColor: '#0080ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5
+  },
+  regisText: {
+    color: '#fff',
+    fontWeight: 'bold'
   }
 })
